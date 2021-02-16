7016fdd1714def72fef8d6fb1c6c388c9d8c57be
Alexgub84 <alex.gub@gmail.com>
2021-02-16 14:45:24 +0200

    first

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./assets/css/main.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">React Google Maps</header>
+      <main></main>
+      <footer>Alex Guberman</footer>
+    </div>
+  );
+}
+
+export default App;
